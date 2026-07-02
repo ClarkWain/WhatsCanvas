34ab0e0203a1892d867d3e731c95da36a408f253
eieihihi <eieihihi@outlook.com>
2026-07-02 16:47:17 +0800

    Vendor Polyline2D into third_party and drop submodule

--- cmake/WhatsCanvasOpenGL.cmake
+++ cmake/WhatsCanvasOpenGL.cmake
@@ -3,6 +3,7 @@ function(whatscanvas_add_common_dependencies project_root)
     set(glad_path "${third_party_dir}/glad")
     set(stb_path "${third_party_dir}/stb")
     set(glm_path "${third_party_dir}/glm")
+    set(polyline2d_path "${third_party_dir}/polyline2d")
 
     if (NOT EXISTS "${glad_path}/src/glad.c" OR
         NOT EXISTS "${glad_path}/include/glad/glad.h" OR
@@ -31,7 +32,7 @@ function(whatscanvas_add_common_dependencies project_root)
 
     if (NOT TARGET WhatsCanvasPolyline2D)
         add_library(WhatsCanvasPolyline2D INTERFACE)
-        target_include_directories(WhatsCanvasPolyline2D SYSTEM INTERFACE "${project_root}/include")
+        target_include_directories(WhatsCanvasPolyline2D SYSTEM INTERFACE "${polyline2d_path}/include")
     endif()
 endfunction()
 
